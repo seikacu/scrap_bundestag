dd85cb912cb9c00d4ba728dc1ecd22784e9cdde4
neoxak <neoxak@gmail.com>
2023-06-05 20:32:04 +0300

    complete

--- scrup_bundestag.py
+++ scrup_bundestag.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-
+import json
 # persons_url_list = []
 
 # for i in range(0, 748, 12):
@@ -27,6 +27,9 @@ with open('persons_url_list.txt') as file:
     
     lines = [line.strip() for line in file.readlines()]
     
+    data_dict = []
+    count = 0
+    
     for line in lines:
         q = requests.get(line)
         result = q.content
@@ -37,14 +40,22 @@ with open('persons_url_list.txt') as file:
         person_name = person_name_company[0]
         person_company = person_name_company[1].strip()
         
-# q = requests.get('https://www.bundestag.de/en/members/abdi_sanae-861028')
-# result = q.content
+        social_network = soup.find_all(class_='bt-link-extern')
         
-# soup = BeautifulSoup(result, 'lxml')
-# person = soup.find(class_='bt-biografie-name').find('h3').text
-# person_name_company = person.strip().split(',')
-# person_name = person_name_company[0]
-# person_company = person_name_company[1]
-
-# print(person_name)
-# print(person_company)
+        social_network_urls = []
+        for item in social_network:
+            social_network_urls.append(item.get('href'))
+        
+        data = {
+            'person_name': person_name,
+            'company_name': person_company,
+            'social_networks': social_network_urls
+        }
+        count += 1
+        print(f'#{count}: {line} is done!')
+        
+        data_dict.append(data)
+        
+        with open('data.json', 'w') as json_file:
+            json.dump(data_dict, json_file, indent=4)
+            
